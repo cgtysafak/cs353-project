dffdeb2c61d596280f09bf0718675c9af117463e
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2023-05-12 20:11:54 +0300

    django app creation

---
+++ backend/careersite/career/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+
+from . import views
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('home/', views.HomeView.as_view(),  name="home"),
+]
---
+++ backend/careersite/career/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.db import connection
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views import View
+# Create your views here.
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'career/home.html')
+
+
+
+#class LoginView(View):
+#    def get(self, request):
+
+
+
+
+
+def login(request):
+    if request.method == 'POST':
+        # get username and password from front-end
+        post = request.POST
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        t = request.POST.get("type", "")
+        # check if user exists if exists and password is correct send to index, if not show a warning
+        try:
+            stmt = "SELECT u_id, username, pw FROM " + t + " WHERE username = '" + username + "' AND pw = '" + password +"'"
+            cursor = connection.cursor()
+            cursor.execute(stmt)
+            r = cursor.fetchone()
+            cursor.close()
+        except:
+            print("db not exist")
+            return render(request, 'travel/Login.html')
+
+
+        if (r != None):
+            request.session['username'] = username
+            request.session['u_id'] = r[0]
+            request.session[t] = True
+            request.session['type'] = t
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, 'travel/Login.html')
+    else:
+        return render(request, 'travel/Login.html')
