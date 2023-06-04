5aadfbf1e23e3588e509bb8498494643ea9eac4b
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2023-06-04 20:28:10 +0300

    logout bug fixed

--- backend/careersite/careersite/context_processors.py
+++ backend/careersite/careersite/context_processors.py
@@ -1,3 +1,7 @@
 def user_id(request):
-    user_id = request.session['user_id']
+    user_id = None
+    if 'user_id' in request.session:
+        user_id = request.session['user_id']
+
     return {'user_id': user_id}
+
